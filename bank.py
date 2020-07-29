37cf85914f129347429039c835677a214ee2a72f
Gavin Quirk <gavinquirk@gmail.com>
2020-07-29 15:24:46 -0700

    new rand num generator, acct balance and balance check, log in function which uses dict of acct objs instead of card nums

--- bank.py
+++ bank.py
@@ -1,25 +1,39 @@
 import random
 
 # All cards which have been created
-all_cards = {}
-current_account = 'none'
+current_account = None
+all_accounts = {}
 
 
 class Account:
     def __init__(self):
         # Generate PIN number
-        self.pin = random.randint(10**(4-1), (10**4)-1)
+        self.pin = gen_rand_number_given_size(4)
         # Generate card number
         self.iin = 400000
-        self.can = random.randint(10**(16-1), (10**16)-1)
+        self.can = gen_rand_number_given_size(9)
         self.checksum = 1  # Will need to generate later using Luhn algo
         self.card_num = int(str(self.iin) + str(self.can) + str(self.checksum))
-        all_cards.update({self.card_num: self.pin})
+        self.balance = 0
+        all_accounts.update({self.card_num: self})
+
+
+def gen_rand_number_given_size(digit_size):
+    return int(''.join(['{}'.format(random.randint(0, 9)) for num in range(0, digit_size)]))
+
+
+def log_in(card_num, pin):
+    if all_accounts[card_num].pin == pin:
+        global current_account
+        current_account = all_accounts[card_num]
+        print('You have successfully logged in!')
+    else:
+        print('Wrong card number or PIN!')
 
 
 while True:
     # Present user with menu
-    if current_account == 'none':
+    if current_account == None:
         print('1. Create an account')
         print('2. Log into account')
         print('0. Exit')
@@ -34,27 +48,24 @@ while True:
         print("Bye!")
         break
     # Create account condition
-    elif user_input == "1" and current_account == 'none':
+    elif user_input == "1" and current_account == None:
         new_account = Account()
-        current_account = new_account.card_num
         print("Your card has been created")
         print("Your card number:")
         print(new_account.card_num)
         print("Your card PIN:")
         print(new_account.pin)
     # Log in condition
-    elif user_input == "2" and current_account == 'none':
+    elif user_input == "2" and current_account == None:
         print('Enter your card number:')
         card_num = int(input())
         print('Enter your PIN:')
         pin = int(input())
-        # If card number is found, and pin is correct, log in
-        if card_num in all_cards and all_cards[card_num] == pin:
-            current_account = card_num
-            print('You have successfully logged in!')
-        else:
-            print('Wrong card number or PIN!')
+        log_in(card_num, pin)
     # Log out condition
-    elif user_input == '2' and current_account != 'none':
-        current_account = 'none'
+    elif user_input == '2' and current_account != None:
+        current_account = None
         print('You have successfully logged out!')
+    # Check balance condition
+    elif user_input == '1' and current_account != None:
+        print('Balance: ' + str(current_account.balance))
