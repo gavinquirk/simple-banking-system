64ebf7706697576511afc058cfd6451093ccf8b8
Gavin Quirk <gavinquirk@gmail.com>
2020-07-28 18:05:16 -0700

    log in and log out

--- bank.py
+++ bank.py
@@ -42,3 +42,19 @@ while True:
         print(new_account.card_num)
         print("Your card PIN:")
         print(new_account.pin)
+    # Log in condition
+    elif user_input == "2" and current_account == 'none':
+        print('Enter your card number:')
+        card_num = int(input())
+        print('Enter your PIN:')
+        pin = int(input())
+        # If card number is found, and pin is correct, log in
+        if card_num in all_cards and all_cards[card_num] == pin:
+            current_account = card_num
+            print('You have successfully logged in!')
+        else:
+            print('Wrong card number or PIN!')
+    # Log out condition
+    elif user_input == '2' and current_account != 'none':
+        current_account = 'none'
+        print('You have successfully logged out!')
